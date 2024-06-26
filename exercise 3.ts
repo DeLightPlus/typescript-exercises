4ebe89f59baf39cf8c3cdbd406ba32e1d447f3b1
DeLightPlus <110693341+DeLightPlus@users.noreply.github.com>
2024-06-26 13:52:17 +0200

    Update exercise 3.ts

--- exercise 3.ts
+++ exercise 3.ts
@@ -1,12 +1,18 @@
+// function that adds two numbers
 function Add(a: number, b: number): number 
 {
     return a + b;
 }
+console.log( Add(6,2) );
 
+
+//Function that returns a fixed number => 0
 function FixedNumber(): number 
 {
     return 0;
 }
+console.log( FixedNumber() );
+
 
 function StringToUpperCase(text: string, toChange?: boolean): string 
 {
@@ -15,3 +21,6 @@ function StringToUpperCase(text: string, toChange?: boolean): string
      else 
         return text.toLowerCase();    
 }
+
+console.log("iscapitalletters");
+console.log("iscapitalletters", true);
